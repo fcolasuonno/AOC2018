a74b484b5085bf7e7172b61d75c910a70572cdb1
Francesco Colasuonno <fcolasuonno@gmail.com>
2018-12-06 15:14:04 +0000

    Better implementation for day 5

--- src/day5/Code.kt
+++ src/day5/Code.kt
@@ -1,37 +1,41 @@
 package day5
 
 import java.io.File
+import java.util.*
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     val name = if (false) "test.txt" else "input.txt"
     val dir = ::main::class.java.`package`.name
     val input = File("src/$dir/$name").readLines()
     val parsed = parse(input)
-    println("Part 1 = ${part1(parsed)}")
-    println("Part 2 = ${part2(parsed)}")
+    println(measureTimeMillis {
+        println("Part 1 = ${part1(parsed)}")
+    })
+    println(measureTimeMillis {
+        println("Part 2 = ${part2(parsed)}")
+    })
 }
 
 fun parse(input: List<String>) = input.first().toList()
 
-fun part1(input: List<Char>): Int {
-    val reduced = input.toMutableList()
-    var doWork: Boolean
-    do {
-        doWork = false
-        for (i in (reduced.size - 2) downTo 0) {
-            val next = reduced.getOrNull(i + 1)
-            if (reduced[i].toLowerCase() == next?.toLowerCase()
-                    && reduced[i] != next) {
-                reduced.removeAt(i)
-                reduced.removeAt(i)
-                doWork = true
-            }
+fun part1(input: List<Char>, exclude: Char? = null): Int {
+    val stack = ArrayDeque<Char>(input.size)
+    for (c in input) {
+        val lowerCased = c.toLowerCase()
+        if (lowerCased == exclude) {
+            continue
         }
-    } while (doWork)
-    return reduced.size
+        if (lowerCased == stack.peek()?.toLowerCase() && c != stack.peek()) {
+            stack.pop()
+        } else {
+            stack.push(c)
+        }
+    }
+    return stack.size
 }
 
 fun part2(input: List<Char>) = ('a'..'z').map { c ->
-    part1(input.filterNot { it.toLowerCase() == c })
+    part1(input, c)
 }.min()
 
