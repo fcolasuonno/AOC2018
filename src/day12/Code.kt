930085556a303e5f3b336d1420d6a4014b00f869
Francesco Colasuonno <fcolasuonno@gmail.com>
2018-12-12 17:37:28 +0000

    Day 12 now with predictions!

--- src/day12/Code.kt
+++ src/day12/Code.kt
@@ -19,15 +19,15 @@ private val lineStructure = """([.#][.#][.#][.#][.#]) => ([.#])""".toRegex()
 
 fun parse(input: List<String>) = input.partition { it.startsWith("initial") }.let {
     Life(
-        it.first.single().let {
-            "[^.#]+([.#]+)".toRegex().matchEntire(it)!!.destructured.component1().toCharArray().map { it == '#' }
-        },
-        it.second.map {
-            lineStructure.matchEntire(it)?.destructured?.let {
-                val (pattern, alive) = it.toList()
-                pattern.toCharArray().map { it == '#' }.takeIf { alive == "#" }
-            }
-        }.filterNotNull().toSet()
+            it.first.single().let {
+                "[^.#]+([.#]+)".toRegex().matchEntire(it)!!.destructured.component1().toCharArray().map { it == '#' }
+            },
+            it.second.map {
+                lineStructure.matchEntire(it)?.destructured?.let {
+                    val (pattern, alive) = it.toList()
+                    pattern.toCharArray().map { it == '#' }.takeIf { alive == "#" }
+                }
+            }.filterNotNull().toSet()
     )
 }
 
@@ -42,18 +42,81 @@ fun part1(input: Life): Int {
 }
 
 fun part2(input: Life): Long {
-    var reachedStabilityCount = 0
-    var prevIndices = emptyList<Int>()
-    var newIndices = listOf(Int.MIN_VALUE)
     var state = List(NegativeConsidered) { false } + input.initial
-
-    while (prevIndices != newIndices) {
+    val box = LinearIntegerRegression(10)
+    while (box.error > 0.1) {
         state = (listOf(false, false) + state + listOf(false, false, false)).windowed(5, 1) {
             input.rules.contains(it)
         }
-        prevIndices = newIndices
-        reachedStabilityCount++
-        newIndices = state.indices.filter { state[it] }.map { it - reachedStabilityCount - NegativeConsidered }
+        box.add(state.indices.filter { state[it] }.map { it - NegativeConsidered }.sum().toLong())
     }
-    return prevIndices.map { it + 50000000000L }.sum()
+    return box.predict(50000000000L - 1)
+}
+
+class LinearRegression(private val sampleNum: Int) {
+    private val samples = mutableListOf<Pair<Double, Double>>()
+    private var currX = 0
+    var b = 0.0
+        private set
+    var a = 0.0
+        private set
+    var error = Double.MAX_VALUE
+        private set
+
+    fun add(y: Double) = add((currX++).toDouble(), y)
+
+    fun add(x: Double, y: Double) {
+        samples.add(Pair(x, y))
+        if (samples.size > sampleNum) {
+            samples.removeAt(0)
+        }
+        val n = samples.size
+        if (n > 2) {
+
+            val sxy = samples.sumByDouble { it.first * it.second }
+            val sx = samples.sumByDouble { it.first }
+            val sx2 = samples.sumByDouble { it.first * it.first }
+            val sy = samples.sumByDouble { it.second }
+            val sy2 = samples.sumByDouble { it.second * it.second }
+            b = (n * sxy - sx * sy) / (n * sx2 - sx * sx)
+            a = (sy - b * sx) / n
+            error = (n * sy2 - sy * sy - b * b * n * sx2 + b * b * sx * sx) / (n * (n - 2))
+        }
+    }
+
+    fun predict(x: Double): Double = a + b * x
+}
+
+class LinearIntegerRegression(private val sampleNum: Int) {
+    private val samples = mutableListOf<Pair<Int, Long>>()
+    private var currX = 0
+    var b = 0.0
+        private set
+    var a = 0.0
+        private set
+    var error = Double.MAX_VALUE
+        private set
+
+    fun add(y: Long) = add(currX++, y)
+
+    fun add(x: Int, y: Long) {
+        samples.add(Pair(x, y))
+        if (samples.size > sampleNum) {
+            samples.removeAt(0)
+        }
+        val n = samples.size
+        if (n > 2) {
+
+            val sxy = samples.map { it.first * it.second }.sum()
+            val sx = samples.sumBy { it.first }
+            val sx2 = samples.sumBy { it.first * it.first }
+            val sy = samples.map { it.second }.sum()
+            val sy2 = samples.map { it.second * it.second }.sum()
+            b = (n * sxy - sx * sy).toDouble() / (n * sx2 - sx * sx)
+            a = (sy - b * sx) / n
+            error = (n * sy2 - sy * sy - b * b * n * sx2 + b * b * sx * sx) / (n * (n - 2))
+        }
+    }
+
+    fun predict(x: Long) = (a + b * x).toLong()
 }
